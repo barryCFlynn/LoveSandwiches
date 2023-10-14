bef90717d7c2e31f8dd53006b9393a357ad0bc4c
Barry Flynn <barry.c.flynn@gmail.com>
2023-10-14 23:03:12 +0100

    Add last 5 entries and return them

--- run.py
+++ run.py
@@ -56,6 +56,7 @@ def validate_data(values):
 
     return True
 
+#The functions below were refactored into one, note the additional parameter passed
 
 # def update_sales_worksheet(data):
 #     """
@@ -85,9 +86,6 @@ def update_worksheet(data, worksheet):
     worksheet_to_update.append_row(data)
     print(f"{worksheet} worksheet updated successfully.\n")
 
-
-
-
 def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculate the surplus for each item type.
@@ -107,7 +105,19 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
 
-
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list.
+    """
+    sales = SHEET.worksheet("sales")
+    columns = []
+    for ind in range(1, 7):
+            column = sales.col_values(ind)
+            columns.append(column[-5:])
+    
+    return columns
 
 
 def main():
@@ -121,4 +131,5 @@ def main():
     update_worksheet(new_surplus_data, "surplus")
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+# main()
+sales_columns = get_last_5_entries_sales()
