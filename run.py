46ba703c365988f3bc05eb903ae968986c2dff5f
Barry Flynn <barry.c.flynn@gmail.com>
2023-10-14 20:59:41 +0100

    Add update to gsheet

--- run.py
+++ run.py
@@ -1,52 +1,24 @@
-# import gspread
-# from google.oauth2.service_account import Credentials
+import gspread
+from google.oauth2.service_account import Credentials
 
-# SCOPE = [
-#     "https://www.googleapis.com/auth/spreadsheets",
-#     "https://www.googleapis.com/auth/drive.file",
-#     "https://www.googleapis.com/auth/drive"
-#     ]
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
 
-# CREDS = Credentials.from_service_account_file('creds.json')
-# SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-# GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-# SHEET = GSPREAD_CLIENT.open('love_sandwiches')
-
-# sales = SHEET.worksheet('sales')
-
-# data = sales.get_all_values()
-
-data_dict = {
-    'sales': {
-        'cheese ham': [28, 26, 28, 25, 22, 27, 28],
-        'tom moz': [31, 32, 28, 32, 28, 27, 26],
-        'chicken salad': [21, 33, 30, 31, 30, 36, 32],
-        'egg salad': [23, 25, 22, 24, 23, 26, 28],
-        'hummus veg': [30, 28, 29, 19, 33, 32, 26],
-        'ham egg': [30, 27, 26, 30, 28, 31, 31]
-    },
-    'surplus': {
-        'cheese ham': [4, 4, -6, -2, -7, 3, 0],
-        'tom moz': [-5, 0, 0, -4, 0, 5, -6],
-        'chicken salad': [5, -2, -3, 0, 3, 5, 6],
-        'egg salad': [-8, -4, 3, 0, -3, 4, -5],
-        'hummus veg': [0, -2, 6, 0, 10, 2, 7],
-        'ham egg': [4, -6, 8, 5, 0, 2, 5]
-    },
-    'stock': {
-        'cheese ham': [24, 22, 34, 27, 29, 24, 28, 28],
-        'tom moz': [36, 32, 28, 36, 28, 23, 32, 31],
-        'chicken salad': [16, 35, 33, 31, 27, 31, 26, 33],
-        'egg salad': [31, 29, 19, 24, 26, 22, 33, 29],
-        'hummus veg': [30, 30, 23, 19, 23, 30, 19, 22],
-        'ham egg': [26, 33, 18, 25, 28, 29, 26, 31]
-    }
-}
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 
 def get_sales_data():
     """
-    Get sales figures input from the user
+    Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
@@ -54,7 +26,7 @@ def get_sales_data():
         print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here: ")
-        
+
         sales_data = data_str.split(",")
 
         if validate_data(sales_data):
@@ -63,9 +35,12 @@ def get_sales_data():
 
     return sales_data
 
+
 def validate_data(values):
     """
-    to validate the data entered by the user
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
     """
     try:
         [int(value) for value in values]
@@ -79,23 +54,17 @@ def validate_data(values):
 
     return True
 
+
 def update_sales_worksheet(data):
     """
     Update sales worksheet, add new row with the list data provided
     """
-    updated_data_dict = data_dict.copy()
     print("Updating sales worksheet...\n")
-    for key in data_dict['sales']:
-                updated_data_dict['sales'][key] = data_dict['sales'][key] + sales_data
-
-    # sales_worksheet = SHEET.worksheet("sales")
-    # sales_worksheet.append_row(data)
-    # print("Sales worksheet updated successfully.\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
 
 
 data = get_sales_data()
 sales_data = [int(num) for num in data]
 update_sales_worksheet(sales_data)
-print(data_dict['sales'])
-
-
